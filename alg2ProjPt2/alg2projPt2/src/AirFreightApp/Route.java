8e4d3c51be9dd40b5560ee21d79a1a05235d7dfc
Lujain Aljahdali <134057193+LujainWaleed@users.noreply.github.com>
2023-06-04 21:15:43 +0300

    Route.java

--- alg2ProjPt2/alg2projPt2/src/AirFreightApp/Route.java
+++ alg2ProjPt2/alg2projPt2/src/AirFreightApp/Route.java
@@ -4,18 +4,14 @@ import GraphFramework.Edge;
 import GraphFramework.Vertex;
 
 public class Route extends Edge {
-//data field
-private int LLength;
 
-//constructor
-public Route(Vertex Source ,Vertex Target ,int weight) {
-    super(Source ,Target ,weight);
-    this.LLength= weight*5; }
+
 
 
 //--------------------------------------------------
 //DisplayInfo method 
 @Override
-public void displayInfo(){ System.out.print(" : line length: "+ LLength); }
+public void displayInfo(){
+}
 
 } 
