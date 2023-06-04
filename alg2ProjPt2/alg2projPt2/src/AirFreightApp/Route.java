3c265eb1aa33ea3a0ca5424615da400e8debecc5
Lujain Aljahdali <134057193+LujainWaleed@users.noreply.github.com>
2023-06-04 22:37:02 +0300

    Route.java

--- alg2ProjPt2/alg2projPt2/src/AirFreightApp/Route.java
+++ alg2ProjPt2/alg2projPt2/src/AirFreightApp/Route.java
@@ -4,14 +4,17 @@ import GraphFramework.Edge;
 import GraphFramework.Vertex;
 
 public class Route extends Edge {
+  
+  //constructor
+  public Route (Vertex Source, Vertex Target, int weight){ super(Source, Target, weight); }
 
 
 
 
-//--------------------------------------------------
-//DisplayInfo method 
-@Override
-public void displayInfo(){
-}
+  //--------------------------------------------------
+  //DisplayInfo method 
+  @Override
+  public void displayInfo(){
+  }
 
 } 
